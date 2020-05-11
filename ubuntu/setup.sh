a431fe41e7faa2e5d1e9438d8f8d87897b7cdcd9
Hangyu Feng <vailgrass@gmail.com>
2020-05-11 18:47:38 -0400

    update script

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -13,12 +13,12 @@ cp ./.vimrc ./.zshrc ~
 git config --global user.email "vailgrass@gmail.com"
 git config --global user.name "Hangyu Feng"
 
+# vim setup
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
 # zsh setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -L git.io/antigen > antigen.zsh  # zsh package manager
 chsh -s $(which zsh)
-
-# vim setup
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
 
