e10eac6944c280dc6df7bf673fc14fadd86bb3d6
Hangyu Feng <vailgrass@gmail.com>
2020-05-11 18:28:15 -0400

    ubuntu setup script

---
+++ ubuntu/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e  # exit whenever a command failed
+
+# install basic programs
+apt-get update
+sudo apt-get install curl wget zsh git vim
+
+# config files
+cp ./.vimrc ./.zshrc ~
+
+# git
+git config --global user.email "vailgrass@gmail.com"
+git config --global user.name "Hangyu Feng"
+
+# zsh setup
+chsh -s $(which zsh)
+if [[ ! $SHELL =~ zsh ]]; then
+  echo "zsh version:"
+  zsh --version
+  echo "either zsh isn't installed or isn't set as default"
+  exit 1
+fi
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -L git.io/antigen > antigen.zsh  # zsh package manager
+source ~/.zshrc
+
+# vim setup
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
