eeb4d4551176b3737b0be31ba37de6776d91bb78
Hangyu Feng <vailgrass@gmail.com>
2020-05-11 19:08:09 -0400

    update ubuntu setup script

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -8,17 +8,26 @@ sudo apt install curl wget zsh git vim-gtk3  # vim-gtk3 provides extended featur
 
 # config files
 cp ./.vimrc ./.zshrc ~
+cd ~
 
 # git
 git config --global user.email "vailgrass@gmail.com"
 git config --global user.name "Hangyu Feng"
 
 # vim setup
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ -d "~/.vim/bundle/Vundle.vim" ]; then
+  cd ~/.vim/bundle/Vundle.vim && git pull && cd ~
+else
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 vim +PluginInstall +qall
 
 # zsh setup
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ||
-curl -L git.io/antigen > antigen.zsh  # zsh package manager
+if [ ! -d "~/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+if [ ! -d "~/antigen.zsh" ]; then
+  curl -L git.io/antigen > ~/antigen.zsh  # zsh package manager
+fi
 chsh -s $(which zsh) && zsh
 
