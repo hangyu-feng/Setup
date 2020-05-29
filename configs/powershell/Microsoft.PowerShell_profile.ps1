4fa691065b787828c20714bd5264c568b6c221ee
Hangyu Feng <vailgrass@gmail.com>
2020-05-28 23:11:34 -0400

    windows terminal and powershell setup

---
+++ configs/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Agnoster  # More themes at https://github.com/JanDeDobbeleer/oh-my-posh#themes
---
+++ configs/powershell/scripts/install_powerline.ps1
@@ -0,0 +1,12 @@
+# Using PowerShell, install Posh-Git and Oh-My-Posh.
+# Posh-Git adds Git status information to your prompt as well as tab-completion
+# for Git commands, parameters, remotes, and branch names. Oh-My-Posh provides
+# theme capabilities for your PowerShell prompt.
+Install-Module posh-git -Scope CurrentUser
+Install-Module oh-my-posh -Scope CurrentUser
+
+# If you are using PowerShell Core, install PSReadline.
+# PSReadline lets you customize the command line editing environment in PowerShell.
+Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+
+# Here I installed both posh-git and PSReadline because there is nothing wrong.
