da5036388162fe36ed2db62781cfd77a2229eceb
Hangyu Feng <hfeng@financeit.io>
2020-05-30 16:28:39 -0400

    update powershell

--- configs/powershell/Microsoft.PowerShell_profile.ps1
+++ configs/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,13 @@
+# To use this profile, dot source it in $profile like this:
+# . ~/setup/configs/powershell/Microsoft.PowerShell_profile.ps1
+
 Import-Module posh-git
 Import-Module oh-my-posh
 
 Set-Theme Agnoster  # More themes at https://github.com/JanDeDobbeleer/oh-my-posh#themes
 
-. ~/setup/configs/powershell/scripts/git_alias.ps1
+. $PSScriptRoot/scripts/git_alias.ps1
+
+# Some other aliases
+function reload { . ${profile} }
+function scst { scoop update && scoop status }
