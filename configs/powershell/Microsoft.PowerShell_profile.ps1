84ffb70edc74c6c54d1d58736bc94d0e3b725ae0
Hangyu Feng <hfeng@financeit.io>
2020-05-30 01:08:13 -0400

    git aliases

--- configs/powershell/Microsoft.PowerShell_profile.ps1
+++ configs/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
 Import-Module posh-git
 Import-Module oh-my-posh
+
 Set-Theme Agnoster  # More themes at https://github.com/JanDeDobbeleer/oh-my-posh#themes
+
+. ~/setup/configs/powershell/scripts/git_alias.ps1
---
+++ configs/powershell/scripts/git_alias.ps1
@@ -0,0 +1,32 @@
+# Here are some git aliases I borrowed from oh-my-zsh's git plugin:
+# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git
+
+set-alias -name g -value git
+function ga { git add }
+function gaa { git add --all }
+function gav { git add --verbose }
+function gb { git branch }
+function gc { git commit -v }
+function gc! {  git commit --amend }
+function gcam { git commit -a -m $args }
+function gcb { git checkout -b }
+function gcf { git config --list }
+function gcm { git checkout master }
+function gco { git checkout }
+function gcp { git cherry-pick }
+function gcpa {	git cherry-pick --abort }
+function gcpc {	git cherry-pick --continue }
+function gd { git diff }
+function gf { git fetch }
+function gfa { git fetch --all --prune }
+function gl { git pull }
+function gm { git merge }
+function gmom { git merge origin/master }
+function gma { git merge --abort }
+function gp { git push }
+function grs { git restore }
+function gst { git status }
+function gstc { git stash clear }
+function gstd { git stash drop }
+function gstl { git stash list }
+function gstp { git stash pop }
