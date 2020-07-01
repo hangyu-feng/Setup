887a925a2835b9205216f717e7070615a2f18bfc
Hangyu Feng <hfeng@financeit.io>
2020-07-01 19:51:57 -0400

    args

--- configs/powershell/scripts/git_alias.ps1
+++ configs/powershell/scripts/git_alias.ps1
@@ -3,37 +3,37 @@
 
 set-alias -name g -value git
 
-function current_branch { git rev-parse --abbrev-ref HEAD }
+function current_branch { git rev-parse --abbrev-ref HEAD $args }
 set-alias -name git_current_branch -value current_branch
-function ga { git add }
-function gaa { git add --all }
-function gav { git add --verbose }
-function gb { git branch }
-function gc { git commit -v }
-function gc! {  git commit --amend }
+function ga { git add $args }
+function gaa { git add --all $args }
+function gav { git add --verbose $args }
+function gb { git branch $args }
+function gc { git commit -v $args }
+function gc! {  git commit --amend $args }
 function gcam { git commit -a -m $args }
 function gcb { git checkout -b $args }
-function gcf { git config --list }
-function gcm { git checkout master }
+function gcf { git config --list $args }
+function gcm { git checkout master $args }
 function gco { git checkout $args }
-function gcp { git cherry-pick }
-function gcpa {	git cherry-pick --abort }
-function gcpc {	git cherry-pick --continue }
-function gd { git diff }
-function gf { git fetch }
-function gfa { git fetch --all --prune }
-function ggu { git pull --rebase origin $(current_branch) }
-function gpsup { git push --set-upstream origin $(current_branch) }
-function gpull { git pull }
-function gm { git merge }
-function gmom { git merge origin/master }
-function gma { git merge --abort }
-function gp { git push }
-function grhh { git reset HEAD }
-function grhh { git reset --hard HEAD }
-function grs { git restore }
-function gst { git status }
-function gstc { git stash clear }
-function gstd { git stash drop }
-function gstl { git stash list }
-function gstp { git stash pop }
+function gcp { git cherry-pick $args }
+function gcpa {	git cherry-pick --abort $args }
+function gcpc {	git cherry-pick --continue $args }
+function gd { git diff $args }
+function gf { git fetch $args }
+function gfa { git fetch --all --prune $args }
+function ggu { git pull --rebase origin $(current_branch) $args }
+function gpsup { git push --set-upstream origin $(current_branch) $args }
+function gpull { git pull $args }
+function gm { git merge $args }
+function gmom { git merge origin/master $args }
+function gma { git merge --abort $args }
+function gp { git push $args }
+function grhh { git reset HEAD $args }
+function grhh { git reset --hard HEAD $args }
+function grs { git restore $args }
+function gst { git status $args }
+function gstc { git stash clear $args }
+function gstd { git stash drop $args }
+function gstl { git stash list $args }
+function gstp { git stash pop $args }
